73923b406fa7f662093e0890e4d1917bcda80877
Jonathan Karlsson <karlsjona@gmail.com>
2023-04-08 14:19:30 +0200

    Add tests for LDA

--- nestruts/test/cpu.cpp
+++ nestruts/test/cpu.cpp
@@ -14,6 +14,35 @@ TEST_CASE("LDA Immediate", "[instruction]") {
     REQUIRE(cpu->get_acc() == 0x11);
 }
 
+TEST_CASE("LDA Absolute", "[instruction]") {
+    auto m = std::make_unique<memory_bus>(nullptr, nullptr);
+    m->write(0x0000, 0xAD); // LDA Absolute
+    m->write(0x0001, 0x34); // addr low
+    m->write(0x0002, 0x12); // addr high
+    m->write(0x1234, 0xAB); // value to read
+    auto cpu = std::make_unique<core6502>(
+        std::move(m), [] { return false; }, [] { return false; });
+    cpu->setpp(0x0000);
+    cpu->cycle();
+    REQUIRE(cpu->get_acc() == 0xAB);
+}
+
+TEST_CASE("LDA X-Indexed Absolute", "[instruction]") {
+    auto m = std::make_unique<memory_bus>(nullptr, nullptr);
+    m->write(0x0000, 0xA2); // LDX immediate
+    m->write(0x0001, 0x10); // value
+    m->write(0x0002, 0xBD); // LDA X-Indexed Absolute
+    m->write(0x0003, 0x34); // addr low
+    m->write(0x0004, 0x12); // addr high
+    m->write(0x1244, 0xCD); // value to read
+    auto cpu = std::make_unique<core6502>(
+        std::move(m), [] { return false; }, [] { return false; });
+    cpu->setpp(0x0000);
+    cpu->cycle(); // LDX 0x10
+    cpu->cycle(); // LDA
+    REQUIRE(cpu->get_acc() == 0xCD);
+}
+
 TEST_CASE("ADC Immediate", "[instruction]") {
     auto m = std::make_unique<memory_bus>(nullptr, nullptr);
     m->write(0x0000, 0xA9); // LDA immediate
@@ -58,8 +87,8 @@ TEST_CASE("PHP", "[instruction]") {
     expected_state.sp--;
     cpu->cycle();
     auto post_state = cpu->dump_state();
-    cpu->cycle();
     REQUIRE(expected_state == post_state);
+    cpu->cycle();
     REQUIRE(cpu->get_acc() == 0x01);
 }
 
